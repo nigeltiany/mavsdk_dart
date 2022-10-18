68627048a24a262bd2a852befd242238c881f2a0
Nigel Tiany <nigeltiany@gmail.com>
2022-10-18 16:45:31 -0400

    Remove unused code in tests

--- test/mavsdk_dart_test.dart
+++ test/mavsdk_dart_test.dart
@@ -1,16 +1,5 @@
-import 'package:mavsdk_dart/mavsdk.dart';
+import 'package:mavsdk/mavsdk.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
 }
